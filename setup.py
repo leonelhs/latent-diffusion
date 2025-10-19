b3d8be530b6f0c90b759c1b0bcb4981e4aff5a69
leonel <leonelhs@gmail.com>
2025-10-19 15:47:27 -0600

    ready to hf

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup, find_packages
 
-setup(
-    name='latent-diffusion',
-    version='0.0.1',
-    description='',
-    packages=find_packages(),
-    install_requires=[
-        'torch',
-        'numpy',
-        'tqdm',
-    ],
-)
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+def main():
+    setup(
+        name='latent-diffusion',
+        version='0.0.2',
+        description='High-Resolution Image Synthesis with Latent Diffusion Models ',
+        url='https://github.com/CompVis/latent-diffusion',
+        author='Blattmann, Andreas and Rombach, Robin and Oktay, Kaan and Ommer, Björn',
+        author_email='robin.rombach@gmail.com',
+        packages=find_packages(),
+        install_requires=required
+    )
+
+if __name__ == "__main__":
+    main()
